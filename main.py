92bd3a181e48ce09ec3a309d3a1eccff80559651
¯\_(ツ)_/¯ <16128011+SomeShrug@users.noreply.github.com>
2019-07-17 23:25:52 -0500

    Hopefully fix local domain reference issues.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import sys
 
 if __name__ == '__main__':
     path = os.path.dirname(os.path.realpath(__file__))
-    sys.path.insert(0, os.path.join(path, 'src'))
+    sys.path.insert(0, path)
+    sys.path.insert(1, os.path.join(path, 'src'))
+    sys.path.insert(2, os.path.join(path, 'locales'))
     import bot
 
     bot.main()
